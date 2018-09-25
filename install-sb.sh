8a949b95f345c1518d5a9585db89baccb403abdd
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-09-25 09:20:12 +0100

    Add files via upload

---
+++ install-sb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#gmail hack
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+#facebook
+mkdir facebook
+cd facebook
+wget raw.githubusercontent.com/Sup3r-Us3r/scripts/master/fb-brute.pl
+cd ..
+#Instagram
+git clone https://github.com/thelinuxchoice/instainsane.git
+cd instainsane
+chmod +x instainsane.sh
+chmod +x install.sh
+sudo ./install.sh
+cd ..
+#Twitter
+git clone https://github.com/thelinuxchoice/tweetshell.git
+cd tweetshell
+chmod +x tweetshell.sh
+chmod +x install.sh
+sudo ./install.sh
+cd ..
+
